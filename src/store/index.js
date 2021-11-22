43ef58523ec21e8e876a36720e51e615ca9cdf34
sej-dev <wndmstla1@gmail.com>
2021-11-22 14:47:08 +0900

    Feat: vuex3 -> vuex4

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,7 @@
-import Vue  from "vue";
-import Vuex from "vuex";
+import { createStore } from 'vuex';
 import todo from '@/store/modules/todo';
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
+export default createStore({
     modules: {
         todo,
     },
